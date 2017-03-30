f79f59d9e1f1fce9da0752bc696ad752f908b893
Jeffrey D <literallyelvis@gmail.com>
2017-03-29 20:45:18 -0500

    Minor migrations improvements, move product handlers into the products file

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -13,47 +13,25 @@ import (
 var db *pg.DB
 
 // HomeHandler serves up our basic web page
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func HomeHandler(res http.ResponseWriter, req *http.Request) {
 	// vars := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
-
-	rv := struct{ OK bool }{OK: true}
-	json.NewEncoder(w).Encode(rv)
-}
-
-// ProductsHandler is a generic product list response handler
-func ProductsHandler(w http.ResponseWriter, r *http.Request) {
-	switch method := r.Method; method {
-	case "GET":
-		var products []Product
-		err := db.Model(&products).Select()
-		if err != nil {
-			log.Fatalf("Error encountered querying for products: %v", err)
-		}
-		json.NewEncoder(w).Encode(products)
-	case "POST":
-		if r.Body == nil {
-			http.Error(w, "Please send a request body", http.StatusBadRequest)
-			return
-		}
-
-		newProduct := &Product{}
-		err := json.NewDecoder(r.Body).Decode(newProduct)
-		if err != nil {
-			http.Error(w, "Invalid request body", http.StatusBadRequest)
-			return
-			// fmt.Fprintf(w, "Error encountered parsing request: %v", err)
-		}
-
-		err = db.Insert(newProduct)
-		if err != nil {
-			log.Printf("error inserting product into database: %v", err)
-		}
+	if val, ok := req.Header["User-Agent"]; ok {
+		log.Printf("User-Agent: %v", val)
 	}
+	indexPage, err := ioutil.ReadFile("static/index.html")
+	if err != nil {
+		log.Printf("error occurred reading indexPage: %v\n", err)
+	}
+	res.Write(indexPage)
 }
 
 func main() {
 	// init stuff
+	domainName := os.Getenv("DAIRYCART_DOMAIN")
+	if domainName == "" {
+		domainName = "localhost"
+	}
+
 	dbURL := os.Getenv("DAIRYCART_DB_URL")
 	dbOptions, err := pg.ParseURL(dbURL)
 	if err != nil {
@@ -66,6 +44,9 @@ func main() {
 	router.HandleFunc("/products", ProductsHandler).Methods("GET", "POST")
 
 	// Basic business
+	// router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	http.Handle("/", router)
+
+	log.Println("Listening at port 8080")
 	http.ListenAndServe(":8080", nil)
 }
--- product.go
+++ product.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
 // Variant describes children of products with different attributes from the parent
 type Variant struct {
 	ID int64
@@ -30,4 +36,35 @@ type Product struct {
 	Quantity              int32   `json:"quantity"`
 
 	// SalePrice float32
+}
+
+// ProductsHandler is a generic product list response handler
+func ProductsHandler(res http.ResponseWriter, req *http.Request) {
+	switch method := req.Method; method {
+	case "GET":
+		var products []Product
+		err := db.Model(&products).Select()
+		if err != nil {
+			log.Printf("Error encountered querying for products: %v", err)
+		}
+		json.NewEncoder(res).Encode(products)
+	case "POST":
+		if req.Body == nil {
+			http.Error(res, "Please send a request body", http.StatusBadRequest)
+			return
+		}
+
+		newProduct := &Product{}
+		err := json.NewDecoder(req.Body).Decode(newProduct)
+		if err != nil {
+			http.Error(res, "Invalid request body", http.StatusBadRequest)
+			return
+			// fmt.Fprintf(w, "Error encountered parsing request: %v", err)
+		}
+
+		err = db.Insert(newProduct)
+		if err != nil {
+			log.Printf("error inserting product into database: %v", err)
+		}
+	}
 }
