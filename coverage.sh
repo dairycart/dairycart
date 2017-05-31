ce8b840fba23c57009e9792b67984e42682c4993
Jeffrey D <verygoodsoftwarenotvirus@users.noreply.github.com>
2017-05-31 00:06:25 -0500

    Remove likely redundant gitignore entry and improve coverage script
    Now I'm never going to be saving coverage files locally, which is nice.

--- coverage.sh
+++ coverage.sh
@@ -1,2 +1,7 @@
+if [ -f coverage.out ]
+then
+    rm coverage.out
+fi
 go test github.com/verygoodsoftwarenotvirus/dairycart/api -coverprofile=coverage.out
 go tool cover -html=coverage.out
+rm coverage.out
