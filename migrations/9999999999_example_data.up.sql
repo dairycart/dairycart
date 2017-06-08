2ae7dbdf5129c52eead19b0e8c528d49871d850c
Jeffrey D <verygoodsoftwarenotvirus@users.noreply.github.com>
2017-06-08 07:45:49 -0500

    Added product optionst able, revised attributes example data to make sense
    The previous values I had in there were just copied from options (and
    were more appropriately left there). I created some dummy attributes that
    I think are more in line with what I imagine attributes to be, and had to
    revise a bunch of integration tests as a consequence. Only values were
    changed, however, not expectations.

--- migrations/9999999999_example_data.up.sql
+++ migrations/9999999999_example_data.up.sql
@@ -39,17 +39,18 @@ VALUES
 (1, 't-shirt-large-blue', 'Blue T-Shirt (Large)', null, 12.34, 5, 123),
 (2, 'skateboard', 'Skateboard', '1234567890', 99.99, 50, 123);
 
-INSERT INTO product_attributes
+INSERT INTO product_options
 (
-    "name"
+    "name",
+    "product_progenitor_id"
 )
 VALUES
-('color'),
-('size');
+('color', 1),
+('size', 1);
 
-INSERT INTO product_attribute_values
+INSERT INTO product_option_values
 (
-    "product_attribute_id",
+    "product_option_id",
     "value"
 )
 VALUES
@@ -59,6 +60,29 @@ VALUES
 (2, 'small'),
 (2, 'medium'),
 (2, 'large');
+
+INSERT INTO product_attributes
+(
+    "name"
+)
+VALUES
+('Concave Shape'),
+('Ply Count');
+
+INSERT INTO product_attribute_values
+(
+    "product_attribute_id",
+    "value"
+)
+VALUES
+(1, 'Radial'),
+(1, 'Progressive'),
+(1, 'W-Concave'),
+(1, 'Flatcave'),
+(1, 'Flat'),
+(2, '7-ply'),
+(2, '9-ply');
+
 
 INSERT INTO discounts
 (
