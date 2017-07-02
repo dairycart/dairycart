e67e3eb96006e115e0a7535de58785a2a12ed137
Jeffrey D <verygoodsoftwarenotvirus@users.noreply.github.com>
2017-07-02 09:42:27 -0500

    remove main_test.go
    it didn't do what it said it did, so why keep it around

--- api/helpers_test.go
+++ api/helpers_test.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http/httptest"
 	"net/url"
+	"os"
 	"strings"
 	"testing"
 	"time"
 
+	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
+
+	"github.com/go-chi/chi"
+	"github.com/gorilla/sessions"
+	"github.com/jmoiron/sqlx"
+	"github.com/jmoiron/sqlx/reflectx"
 	"github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 
@@ -17,6 +26,9 @@ import (
 )
 
 const (
+	exampleSKU               = "example"
+	exampleTimeString        = "2016-12-01 12:00:00.000000"
+	exampleGarbageInput      = `{"things": "stuff"}`
 	exampleMarshalTimeString = "2016-12-31T12:00:00.000000Z"
 )
 
@@ -25,9 +37,25 @@ var (
 	exampleFilterEndTime   time.Time
 	defaultQueryFilter     *QueryFilter
 	customQueryFilter      *QueryFilter
+
+	arbitraryError   error
+	exampleTime      time.Time
+	exampleOlderTime time.Time
+	exampleNewerTime time.Time
 )
 
 func init() {
+	log.SetOutput(ioutil.Discard)
+	arbitraryError = errors.New("pineapple on pizza")
+
+	var timeParseErr error
+	exampleOlderTime, timeParseErr = time.Parse("2006-01-02 03:04:00.000000", exampleTimeString)
+	if timeParseErr != nil {
+		log.Fatalf("error parsing time")
+	}
+	exampleTime = exampleOlderTime.Add(30 * (24 * time.Hour))
+	exampleNewerTime = exampleTime.Add(30 * (24 * time.Hour))
+
 	defaultQueryFilter = &QueryFilter{
 		Page:  1,
 		Limit: 25,
@@ -39,6 +67,88 @@ func init() {
 		CreatedAfter: generateExampleTimeForTests(),
 	}
 }
+
+///////////////////////////////////////////////////////
+//                                                   //
+//   These functions don't actually anything, but    //
+//   rather contains some small helper functions     //
+//   that might be used by all the tests.            //
+//                                                   //
+///////////////////////////////////////////////////////
+
+type TestUtil struct {
+	Response *httptest.ResponseRecorder
+	Router   *chi.Mux
+	DB       *sqlx.DB
+	Mock     sqlmock.Sqlmock
+	Store    *sessions.CookieStore
+}
+
+func generateExampleTimeForTests() time.Time {
+	t, err := time.Parse("2006-01-02 03:04:00.000000", "2016-12-31 12:00:00.000000")
+	if err != nil {
+		log.Fatalf("error parsing time")
+	}
+	return t
+}
+
+func setExpectationsForRowCount(mock sqlmock.Sqlmock, table string, queryFilter *QueryFilter, count uint64, err error) {
+	exampleRows := sqlmock.NewRows([]string{"count"}).AddRow(count)
+	mock.ExpectQuery(formatQueryForSQLMock(buildCountQuery(table, queryFilter))).
+		WillReturnRows(exampleRows).
+		WillReturnError(err)
+}
+
+func setupTestVariables(t *testing.T) *TestUtil {
+	mockDB, mock, err := sqlmock.New()
+	db := sqlx.NewDb(mockDB, "postgres")
+	db.Mapper = reflectx.NewMapperFunc("json", strings.ToLower)
+	assert.Nil(t, err)
+
+	secret := os.Getenv("DAIRYSECRET")
+	if len(secret) < 32 {
+		log.Fatalf("Something is up with your app secret: `%s`", secret)
+	}
+	store := sessions.NewCookieStore([]byte(secret))
+
+	router := chi.NewRouter()
+	SetupAPIRoutes(router, db, store)
+
+	return &TestUtil{
+		Response: httptest.NewRecorder(),
+		Router:   router,
+		DB:       db,
+		Mock:     mock,
+		Store:    store,
+	}
+}
+
+func formatQueryForSQLMock(query string) string {
+	for _, x := range []string{"$", "(", ")", "=", "*", ".", "+", "?", ",", "-"} {
+		query = strings.Replace(query, x, fmt.Sprintf(`\%s`, x), -1)
+	}
+	return query
+}
+
+func ensureExpectationsWereMet(t *testing.T, mock sqlmock.Sqlmock) {
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expections: %s", err)
+	}
+}
+
+func argsToDriverValues(args []interface{}) []driver.Value {
+	rv := []driver.Value{}
+	for _, x := range args {
+		rv = append(rv, x)
+	}
+	return rv
+}
+
+///////////////////////////////////////////////////////
+//                                                   //
+//        These functions actually test things       //
+//                                                   //
+///////////////////////////////////////////////////////
 
 func TestNullStringMarshalTextReturnsNilIfStringIsInvalid(t *testing.T) {
 	t.Parallel()
