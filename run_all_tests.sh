2a3b9840593bde3fb0d3f9f5ee12cd6b7d893e47
Jeffrey D <verygoodsoftwarenotvirus@users.noreply.github.com>
2017-09-02 06:50:08 -0500

    Fix all the dang tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,3 +1,3 @@
 docker build -t api_test -f api/test.Dockerfile .
 docker run --name api_test --rm api_test
-docker-compose --file docker-compose-test.yml up --abort-on-container-exit --build --remove-orphans --force-recreate
+docker-compose --file api/integration_tests.yml up --abort-on-container-exit --build --remove-orphans --force-recreate
